2bf1bfa37348149965590cdd777a4ec6d2cbfbb2
Md Atifuzzaman <atifuzzaman01@gmail.com>
2021-05-04 13:58:16 +0600

    added readme.md and requirements.txt and some minor improvements

---
+++ README.md
@@ -0,0 +1,20 @@
+# secret_capture
+## Build a Screen recorder using python
+
+
+## Full tutorial (video)
+[![Watch the video-- Build your own screen](https://i.ytimg.com/an_webp/1J8dQA6gN7k/mqdefault_6s.webp?du=3000&sqp=CJ2sw4QG&rs=AOn4CLAgz0Xro-XZWrk3gI99epHO3te9Dg)](https://www.youtube.com/watch?v=1J8dQA6gN7k "Build your own screen recorder")
+
+
+## Installation
+### For windows users
+(run those in command prompt/cmd/terminal)
+
+Download requirements.txt file
+``` 
+pip install -r requirements.txt
+```
+
+
+#### Issues
+If you encounter any problems feel free to open a new issue. Before that check other closed issues and check if your issue matches with any older issues.
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import datetime
-
 from PIL import ImageGrab
 import numpy as np
 import cv2
@@ -9,7 +8,7 @@ width = GetSystemMetrics(0)
 height = GetSystemMetrics(1)
 time_stamp = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
 file_name = f'{time_stamp}.mp4'
-fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 captured_video = cv2.VideoWriter(file_name, fourcc, 20.0, (width, height))
 
 webcam = cv2.VideoCapture(1)
@@ -19,8 +18,9 @@ while True:
     img_np = np.array(img)
     img_final = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
     _, frame = webcam.read()
-    fr_height, fr_width, _ = frame.shape
-    img_final[0:fr_height, 0: fr_width, :] = frame[0: fr_height, 0: fr_width, :]
+    fr_height , fr_width , _ = frame.shape
+    img_final[0:fr_height, 0: fr_width,
+              :] = frame[0: fr_height, 0: fr_width, :]
     cv2.imshow('Secret Capture', img_final)
 
     # cv2.imshow('webcam', frame)
